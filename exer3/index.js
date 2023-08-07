e366d64ea5cb2234bbefe56517d2b11f16aaa6ce
Lucas Santos <lucas22.ferreira.007@gmail.com>
2023-08-07 19:44:14 -0300

    feito o exer3

--- exer3/index.js
+++ exer3/index.js
@@ -1,4 +1,14 @@
+function verificarNumPrimo(num){
+  let divisores=0;
 
+  for(let i=1; i<=num; i++)
+    if(num % i === 0) divisores++;
+
+    if(divisores==2) console.log(`Você digitou ${num}, ele é primo.`);
+    else console.log(`Você digitou ${num}, ele não é primo`);
+}
+
+verificarNumPrimo(5)
 
 // 3. Crie uma função que recebe por parâmetro um valor inteiro e
 // positivo e retorne verdadeiro se o número for primo ou falso se não
