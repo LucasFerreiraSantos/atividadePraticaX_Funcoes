b3d77430e87bb5e7abc92f4d53386dae36d992c9
Lucas Santos <lucas22.ferreira.007@gmail.com>
2023-08-07 19:27:26 -0300

    feito o exer1

--- exer1/index.js
+++ exer1/index.js
@@ -1,4 +1,17 @@
+function calcularMedia(nota1, nota2, nota3, letra){
+  if(letra == 'a'){
+    const mediaAritmetica = (nota1 + nota2 + nota3)/3
+    return mediaAritmetica.toFixed(2)
+  }else if(letra == 'p'){
+    const peso1 = 5
+    const peso2 = 3
+    const peso3 = 2
+    const mediaPonderada = (nota1*peso1 + nota2*peso2 + nota3*peso3)/(peso1 + peso2 + peso3)
+    return mediaPonderada.toFixed(2)
+  }
+}
 
+console.log(calcularMedia(8, 9, 6, 'p'))
 
 
 // 1. Crie uma função que recebe 3 notas de um aluno por parâmetro e
