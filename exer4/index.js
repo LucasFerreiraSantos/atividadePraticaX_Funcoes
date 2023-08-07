a39555032d68b55fa0f9b9718b31bd92cb390f7f
Lucas Santos <lucas22.ferreira.007@gmail.com>
2023-08-07 20:02:59 -0300

    feito o exer4

--- exer4/index.js
+++ exer4/index.js
@@ -1,5 +1,12 @@
+function converterSegundosHoras(seg){
+  const horas = Math.floor(seg / 3600)
+  const minutos = Math.floor((seg % 3600) / 60)
+  const segundosRestantes = seg % 60
 
+  return {horas, minutos, segundos: segundosRestantes}
+}
 
+console.log(converterSegundosHoras(3890))
 // 4. Crie uma função que recebe por parâmetro o tempo de duração de
 // uma fábrica expressa em segundos e retorna esse tempo em horas,
 // minutos e segundos.
