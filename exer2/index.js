f2987707a0f44380ca363a70835aee78d2ef973a
Lucas Santos <lucas22.ferreira.007@gmail.com>
2023-08-07 19:09:01 -0300

    iniciado o repositório

---
+++ exer2/index.js
@@ -0,0 +1,4 @@
+
+
+// 2. Crie uma função que recebe um valor inteiro e retorne verdadeiro se
+// for ímpar ou falso se for par.
---
+++ exer3/index.js
@@ -0,0 +1,5 @@
+
+
+// 3. Crie uma função que recebe por parâmetro um valor inteiro e
+// positivo e retorne verdadeiro se o número for primo ou falso se não
+// for.
