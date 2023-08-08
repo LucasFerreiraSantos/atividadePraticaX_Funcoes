6080c09bfa890af997bed4f2bb673db6c2cbc05a
Lucas Santos <lucas22.ferreira.007@gmail.com>
2023-08-07 21:44:15 -0300

    feito o exer8

--- exer8/index.js
+++ exer8/index.js
@@ -1,4 +1,9 @@
+function mostrarDobro(num){
+  const dobro = num + num
+  console.log(`O dobro de ${num} é ${dobro}.`)
+}
 
+mostrarDobro(3)
 
 // 8. Crie uma função chamada mostrarDobro(num), que recebe um
 // parâmetro sendo um número inteiro. Esta função será chamada ao
