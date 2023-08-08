6242e91a6ec8bdde56c0b2c6db0b3a10b6dd55f2
Lucas Santos <lucas22.ferreira.007@gmail.com>
2023-08-07 21:55:19 -0300

    feito o exer9 e concluida a lista de exercicios

--- exer9/index.js
+++ exer9/index.js
@@ -1,5 +1,10 @@
+function calcularMedia(nota1, nota2, nota3, nome){
+  const media = (nota1 + nota2 + nota3)/3
+  alert(`${nome}, sua média é de ${media.toFixed(2)}.`)
+  console.log(`Nota 1: ${nota1}, Nota 2: ${nota2}, Nota 3: ${nota3}.`)
+}
 
-
+calcularMedia(10, 7, 8, 'Lucas')
 // 9. Crie uma função chamada calcularMedia(nota1, nota2, nota3,
 //   nome), que recebe 4 parâmetros sendo três inteiros e um texto
 //   (String). Esta função será chamada ao abrir a página e mostrará um
