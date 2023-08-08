75ec1685d6730f5660276da3053757a0b9a9295c
Lucas Santos <lucas22.ferreira.007@gmail.com>
2023-08-07 21:38:25 -0300

    feito o exer7

--- exer7/index.js
+++ exer7/index.js
@@ -1,10 +1,11 @@
-const mensagem = prompt('Qual o nome da aplicação?')
-
 function mostrarMensagem(){
-  console.log()
+  const mensagem = 'Minha Mensagem!'
+  console.log(`Acesso a aplicação ${mensagem}.`)
+  alert(`Bem vindo a aplicação ${mensagem}.`)
 }
 
 mostrarMensagem()
+
 // 7. Crie uma função chamada mostrarMensagem() que não tenha
 // parâmetro. Esta função será chamada ao abrir a página e mostrará
 // uma mensagem no console.log() informando “Acesso à aplicação
